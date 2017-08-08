fdfef4e974ac150fd2f5cdf94d821ef8bd6980d6
Firdos fatma <firdos@playsimple.in>
2017-08-09 00:11:11 +0530

    Using Read/Write lock
    Signed-off-by: Firdos fatma <firdos@playsimple.in>

--- src/main/java/com/LRU/Rest/Service/LRUService.java
+++ src/main/java/com/LRU/Rest/Service/LRUService.java
@@ -1,73 +1,105 @@
 package com.LRU.Rest.Service;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class LRUService {
     int capacity;
-    HashMap<Integer, Node> map = new HashMap<Integer, Node>();
+    private ConcurrentHashMap<Integer, Node> map = new ConcurrentHashMap<Integer, Node>();
     Node head = null;
     Node end = null;
+
+    private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    private Lock readLock = readWriteLock.readLock();
+
+    private Lock writeLock = readWriteLock.writeLock();
 
     public LRUService(int capacity) {
         this.capacity = capacity;
     }
 
     public int getValueByKey(int key) {
-        if (map.containsKey(key)) {
-            Node n = map.get(key);
-            remove(n);
-            setHead(n);
-            return n.value;
+        readLock.lock();
+        Node n = null;
+        try {
+            if (map.containsKey(key)) {
+                n = map.get(key);
+                return n.value;
+            }
+            return -1;
+        } finally {
+            if(n!=null){
+                remove(n);
+                setHead(n);
+            }
+            readLock.unlock();
         }
-        return -1;
     }
 
     public void remove(Node n) {
-        if (n.pre != null) {
-            n.pre.next = n.next;
-        } else {
-            head = n.next;
-        }
+        writeLock.lock();
+        try {
+            if (n.pre != null) {
+                n.pre.next = n.next;
+            } else {
+                head = n.next;
+            }
 
-        if (n.next != null) {
-            n.next.pre = n.pre;
-        } else {
-            end = n.pre;
+            if (n.next != null) {
+                n.next.pre = n.pre;
+            } else {
+                end = n.pre;
+            }
+        } finally {
+            writeLock.unlock();
         }
 
     }
 
     public void setHead(Node n) {
-        n.next = head;
-        n.pre = null;
+        writeLock.lock();
+        try {
+            n.next = head;
+            n.pre = null;
 
-        if (head != null)
-            head.pre = n;
+            if (head != null)
+                head.pre = n;
 
-        head = n;
+            head = n;
 
-        if (end == null)
-            end = head;
+            if (end == null)
+                end = head;
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void setValueByKey(int key, int value) {
-        if (map.containsKey(key)) {
-            Node old = map.get(key);
-            old.value = value;
-            remove(old);
-            setHead(old);
-        } else {
-            Node created = new Node(key, value);
-            if (map.size() >= capacity) {
-                map.remove(end.key);
-                remove(end);
-                setHead(created);
-
+        writeLock.lock();
+        try {
+            if (map.containsKey(key)) {
+                Node old = map.get(key);
+                old.value = value;
+                remove(old);
+                setHead(old);
             } else {
-                setHead(created);
-            }
+                Node created = new Node(key, value);
+                if (map.size() >= capacity) {
+                    map.remove(end.key);
+                    remove(end);
+                    setHead(created);
 
-            map.put(key, created);
+                } else {
+                    setHead(created);
+                }
+
+                map.put(key, created);
+            }
+        } finally {
+            writeLock.unlock();
         }
     }
 }
